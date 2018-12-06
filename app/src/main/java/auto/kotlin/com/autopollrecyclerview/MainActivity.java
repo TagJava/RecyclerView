db38fef6ec6ebf94fa6aebd7f55cf6c319454ea7
Moon ch <changjunjie07@163.com>
2018-12-06 17:20:14 +0800

    commit

---
+++ app/src/main/java/auto/kotlin/com/autopollrecyclerview/MainActivity.java
@@ -0,0 +1,13 @@
+package auto.kotlin.com.autopollrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
